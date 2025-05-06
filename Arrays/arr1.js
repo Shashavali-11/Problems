0edd661feb603f2dc8943a01e839ed32b05b05b9
madhav <uniquemadhav@gmail.com>
2025-05-06 18:36:32 +0530

    correction

--- Arrays/arr1.js
+++ Arrays/arr1.js
@@ -3,14 +3,14 @@ console.log(arr.flat(Infinity))
 
 
 arr = [1,2,3,4,5,[6, [7,[8,9]], [10]],11,12]
-// console.log(met2(arr))
+// console.log(method2(arr))
 
-// function met2(arr){
+// function method2(arr){
 
 //     let out = [];
 //     for(i of arr){
 //         if(Array.isArray(i)){
-//             out = out.concat(met2(i))
+//             out = out.concat(method2(i))
 //         }else{
 //             out.push(i);
 //         }
